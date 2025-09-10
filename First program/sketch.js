cf21474318bcc68d8b471822745e174c9476215f
2202591 <2202591@spsd.sk.ca>
2025-09-10 14:09:34 -0600

    Update sketch.js

--- First program/sketch.js
+++ First program/sketch.js
@@ -1,8 +1,8 @@
 // First project
 // Corbin
 // Sept 9, 2025
-//
-// Interactive Programs
+// 
+// Interactive Programs with p5.js
 
 
 function setup() { // runs once at the start
@@ -11,6 +11,6 @@ function setup() { // runs once at the start
 
 function draw() { //runs over and over running 60 frames per sec
   // background(220);
-  fill(0,mouseX, mouseY);
-  circle(mouseX, mouseY, 30);
+  fill(100,mouseX, mouseY);
+  circle(mouseX, mouseY, 10);
 }
