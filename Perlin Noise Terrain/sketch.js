e18f1297b5fdff7ec11c8f383a6e75a72fda99fb
2202591 <2202591@spsd.sk.ca>
2025-09-29 14:10:17 -0600

    noise terrain generation

--- Perlin Noise Terrain/sketch.js
+++ Perlin Noise Terrain/sketch.js
@@ -1,15 +1,40 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Perlin Noise Terrain Generation
+// Corbin Potter
+// September 29, 2025
+// CS30
 
+let rectWidth = 1;
+let timeY;
+let bigX, bigY;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  timeY = random(0,100);
+  generateTerrain();
+
 }
 
 function draw() {
-  background(220);
+  // background(220);
+}
+
+function generateTerrain() {
+
+  rectMode(CORNERS);
+
+  for (let x = 0; x < width; x += rectWidth) {
+    let rectHeight = noise(timeY);
+    rectHeight = map(rectHeight, 0, 1, 50, height/2);
+    // let rectHeight = random(50,500);
+
+    let x2 = x + rectWidth;
+    let y2 = height - rectHeight
+
+    
+    
+    rect(x, height, x2, y2);
+    timeY += 0.007;
+
+  }
+
 }
