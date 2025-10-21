a5458e9172a6bd12e51a0af8849a290621c55c34
2202591 <2202591@spsd.sk.ca>
2025-10-21 14:10:29 -0600

    carrrrrrrrrrs

--- Cars Cars Cars/sketch.js
+++ Cars Cars Cars/sketch.js
@@ -1,19 +1,30 @@
 // Cars Cars Cars
 
-let carLength = 80;
-let carWidth = 40;
-let truckLength = 120;
-let truckWidth = 40;
+let vehicleLength = 80;
+let vehicleWidth = 40;
+let eastbound = [];
+let westbound = [];
+let myVehicle;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CORNERS);
+  myVehicle = new Vehicle(400, 1);
+  for(let i = 0; i < 20; i++) {
+    eastbound.push(new Vehicle(random(height * 0.2, height * 0.45)), 0);
+  }
+  for(let i = 0; i < 20; i++) {
+    westbound.push(new Vehicle(random(height * 0.55, height * 0.8)), 1);
+  }
+
 }
 
 function draw() {
   background(220);
   makeRoad();
-
+  myVehicle.action();
+  
 }
 
 function makeRoad(){
@@ -26,34 +37,47 @@ function makeRoad(){
 }
 
 class Vehicle{
-  constructor(x, y, direction) {
-    this.x = x; this.y = y;
-    //type 0 = car, type 1 = truck
-    this.type = random(0,1);   this.speed = speed;
-    //false = facing east, true = facing west
+  constructor(y, direction) {
+    this.x = random(width); this.y = y;
+    //type 1 = car, type 2 = truck
+    this.type = random(2);   this.speed = random(1,15);
+    //0 = facing east, 1 = facing west
     this.direction = direction;
     this.c = color(random(255), random(255), random(255));
+  }
 
+  action() {
+    this.move();
+    this.display();
   }
 
   display(){
-    fill(this.c);
     if (this.type === 1) { //truck
-      if (this.direction === false){ //facing east
-        rect(this.x, this.y, );
+      fill(this.c);
+      if (this.direction === 0){ //facing east
+        rect(this.x, this.y, this.x + vehicleLength, this.y - vehicleWidth);
+        rect(this.x + vehicleLength + 5, this.y, this.x + vehicleLength + 25, this.y - truckWidth);
       }
       else{
+        rect(this.x, this.y, this.x - vehicleLength, this.y + vehicleWidth);
+        rect(this.x - vehicleLength - 5, this.y, this.x - vehicleLength - 25, this.y + truckWidth);
 
       }
       
     }
     else {
-      if(this.direction === false) {
-        rect(this.x, this.y, this.x + carLength, this.y + carWidth);
-        rect()
+      fill(255);
+      if(this.direction === 0) {
+        rect(this.x + 10, this.y + 3, this.x + 25, this. y - 43);
+        rect(this.x + vehicleLength - 10, this.y + 3, this.x + vehicleLength - 25, this.y - 43);
+        fill(this.c);
+        rect(this.x, this.y, this.x + vehicleLength, this.y - vehicleWidth);
       }
       else {
-        rect(this.x, this.y, this.x - carLength, this.y - carWidth);
+        rect(this.x - 10, this.y - 3, this.x - 25, this.y + 43);
+        rect(this.x - vehicleLength + 10, this.y - 3, this.x - vehicleLength + 25, this.y + 43);
+        fill(this.c);
+        rect(this.x, this.y, this.x - vehicleLength, this.y + vehicleWidth);
       }
 
     }
@@ -61,4 +85,21 @@ class Vehicle{
 
 
   }
+
+  move() {
+    if (this.direction === 0) {
+      this.x += this.speed;
+      if (this.x > width) {
+        this.x = 0 - 0 - vehicleWidth;
+      }
+    }
+    else {
+      this.x -= this.speed;
+      if (this.x < 0) {
+        this.x = width + vehicleWidth;
+      }
+    }
+
+  }
+
 }
